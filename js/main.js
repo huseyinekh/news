1a1767edb68a838be683cc9909301c450dbc6436
huseyinekh@code.edu.az <huseyinekh@code.edu.az>
2020-09-02 01:16:16 +0400

    reconfigure menu"

--- js/main.js
+++ js/main.js
@@ -11,36 +11,48 @@ let sliderItems = $(".slider_item");
 let sliderRightBtn = $(".slider_right_tool");
 let sliderLeftBtn = $(".slider_left_tool");
 
-let currentSliderNum=0
-let sliderCount = sliderItems.length-1;
+let currentSliderNum = 0;
+let sliderCount = sliderItems.length - 1;
 
-const toRightSlide=()=>{
-  if(currentSliderNum==sliderCount){
-    currentSliderNum=0
-  }else{
-    currentSliderNum=currentSliderNum+1
+const toRightSlide = () => {
+  if (currentSliderNum == sliderCount) {
+    currentSliderNum = 0;
+  } else {
+    currentSliderNum = currentSliderNum + 1;
   }
-  sliderItems.fadeOut('slow')
+  sliderItems.fadeOut("slow");
   setTimeout(() => {
-    $(sliderItems[currentSliderNum]).fadeIn('slow')
+    $(sliderItems[currentSliderNum]).fadeIn("slow");
   }, 300);
-  
-}
+};
 
-sliderRightBtn.click(()=>toRightSlide());
+sliderRightBtn.click(() => toRightSlide());
 
-setInterval(()=>toRightSlide(),5000)
+setInterval(() => toRightSlide(), 5000);
 
 sliderLeftBtn.click((e) => {
-  if(currentSliderNum==0){
-    currentSliderNum=3
-  }else{
-    currentSliderNum=currentSliderNum-1
+  if (currentSliderNum == 0) {
+    currentSliderNum = 3;
+  } else {
+    currentSliderNum = currentSliderNum - 1;
   }
-  sliderItems.fadeOut('slow')
+  sliderItems.fadeOut("slow");
   setTimeout(() => {
-    $(sliderItems[currentSliderNum]).fadeIn('slow')
+    $(sliderItems[currentSliderNum]).fadeIn("slow");
   }, 300);
 });
 
-
+let isToggledMenu = false;
+$(".menu_icon").click((e) => {
+  if (!isToggledMenu) {
+    $(".menu ").css({
+      "max-height": "1000px",
+    });
+    isToggledMenu = true;
+  } else {
+    $(".menu ").css({
+      "max-height": "70px",
+    });
+     isToggledMenu = false;
+  }
+});
